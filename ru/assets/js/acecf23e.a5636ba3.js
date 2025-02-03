d3af7469046078c8e293b24b4eefb8c55e06a71a
comtronics <slhome2010@mail.ru>
2025-02-03 15:56:48 +0500

    Deploy website - based on dd235156da1cbd8283651fc7ca84f61e83c3a7ad

---
+++ ru/assets/js/acecf23e.a5636ba3.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunknew_attributico_su=self.webpackChunknew_attributico_su||[]).push([[1903],{1912:t=>{t.exports=JSON.parse('{"blogBasePath":"/new.attributico.su/ru/blog","blogTitle":"Blog","authorsListPath":"/new.attributico.su/ru/blog/authors"}')}}]);
