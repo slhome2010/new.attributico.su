e6238eb9dfdd01a28fa61fbf2303f6ee48d4bf74
slhome <slhome2010@mail.ru>
2025-02-04 21:10:13 +0500

    Svg size?

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,30 +8,36 @@ import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
 import Translate, { translate } from '@docusaurus/Translate';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">
-          <Translate id="homepage.banner.slogan"
-            description="The homepage slogan message">
-            Attributes - it is easy!
-          </Translate>
-        </p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            <Translate id="homepage.banner.link"
-              description="The homepage link label">
-              Attribut&co Tutorial - 5min ⏱️
+      <div className={clsx('container', styles.heroContainer)}>
+        <div className={styles.heroBoxes}>
+          <img src={useBaseUrl('img/logo-attributico.svg')} className={styles.heroBox} />
+        </div>
+        <div className={styles.heroContent}>
+          <Heading as="h1" className="hero__title">
+            {siteConfig.title}
+          </Heading>
+          <p className="hero__subtitle">
+            <Translate id="homepage.banner.slogan"
+              description="The homepage slogan message">
+              Attributes - it is easy!
             </Translate>
-          </Link>
+          </p>
+          <div className={styles.buttons}>
+            <Link
+              className="button button--secondary button--lg"
+              to="/docs/intro">
+              <Translate id="homepage.banner.link"
+                description="The homepage link label">
+                Attribut&co Tutorial - 5min ⏱️
+              </Translate>
+            </Link>
+          </div>
         </div>
       </div>
     </header>
