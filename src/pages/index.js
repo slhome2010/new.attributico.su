72b21ae6e9e820f6693448645b7de0a45fb8e22d
slhome <slhome2010@mail.ru>
2025-02-05 19:31:53 +0500

    Style by ChatGPT

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,8 +20,9 @@ function HomepageHeader() {
             src={useBaseUrl('img/logo/attributico-red-tree.svg')}
             className={styles.heroBox}
             alt="Attributico Logo"
-            width="200"
-            height="200"
+            width="150"
+            height="198"
+            loading="lazy"
           />
         </div>
         <div className={styles.heroContent}>
